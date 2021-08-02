9fd3560e89c57348bd146b53eae959fc3777861c
dtjldamien <dtjldamien@gmail.com>
2021-08-03 00:37:54 +0800

    feat: remove mock verify server

---
+++ verify-access-node/routes/singPass.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const axios = require("axios");
+
+const router = Router();
+
+router.get("/personas", async (req, res) => {
+  try {
+    axios({
+      method: "get",
+      url: "https://sandbox.api.myinfo.gov.sg/com/v3/person-sample-usage/dpp",
+      responseType: "json",
+    })
+      .then(function (response) {
+        res.status(200).json(response.data.Items);
+      })
+      .catch((error) => {
+        this.errorMessage = error.message;
+        console.error("There was an error!", error);
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
